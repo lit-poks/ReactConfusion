9af95c09c3929fbd4ab8e8d3c32eb4ff8e6d2441
lit-poks <pokhrel.lalit99@gmail.com>
2020-10-03 11:07:49 +0600

    redux complete

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,85 +1,202 @@
-import React from 'react';
-import {Card, CardImg, CardText,CardBody,CardTitle, BreadcrumbItem,Breadcrumb} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React, { useState, Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrumb, Button, Modal, ModalHeader, ModalBody, Row, Col, Label, Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import {Loading} from './LoadingComponent'
 
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => (val) && (val.length >= len);
 
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
 
-    function RenderDish({dish}){
-            
-            const commentlist= dish.map((comm)=>{
-                return(
-                    <ul className="list-unstyled" key={comm.id}>
-                        <li className="justify-content-between">
-                        {comm.comment}
-                        </li>
-                        <li className="justify-content-between">
-                        --{comm.author}, <ReturnDate date={comm.date} />
-                        </li>
-                    </ul>
+        this.state={
+            isModalOpen: false,
+        }
 
-                );
-            });
-            return(
-                <div className="col-sm col-md-5 ml-1">
-                <h4>Comments</h4>
-                        {commentlist}
-                </div>
-                
-            );
-        
-        
+        this.handleSubmit=this.handleSubmit.bind(this);
+        this.toggle=this.toggle.bind(this);
     }
 
-    function RenderMenu({dish}){
-       
-            return(
-                <div className="col-sm col-md-5 m-1">
-                <Card >
-                    <CardImg top src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-                </div>  
-            );  
+    toggle(){
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        })
     }
 
-    function ReturnDate({date}){
-        console.log(date)
-        const month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        const mon=parseInt(date.substring(5,7));
-        var final=month[mon-1]+' ' +date.substring(8,10)+', '+date.substring(0,4);
-        return final;
+    handleSubmit(values){
+        this.toggle()
+        this.props.addComment(this.props.dishId,values.rating, values.author,values.comment)
+    };
+
+
+    render(){
+        return (
+            <>
+            <Button outline color="secondary" onClick={this.toggle}><span className="fa fa-pencil"></span> Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}>Submit Comment</ModalHeader>
+
+                <ModalBody>
+                    <Container>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating">
+                                    Rating
+                                        </Label>
+                                <Control.select model=".rating" name="rating" id="rating"
+                                    className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author"> Your Name</Label>
+                                <Control.text model=".author" name="author"
+                                    className="form-control"
+                                    placeholder="Your Name"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".fullName"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
+                            </Row>
+                            <Row className="form-group">
+                                <Label>Comment</Label>
+                                <Control.textarea model=".comment" name="comment"
+                                    className="form-control"
+                                    rows="6"
+                                />
+                            </Row>
+                            <Row className="form-group">
+                                <Button type="submit" color="primary" className="mt-2">
+                                    Submit
+                                        </Button>
+                            </Row>
+                        </LocalForm>
+                    </Container>
+                </ModalBody>
+            </Modal>
+            </>
+        );
     }
 
-    const DishDetail = (props) =>{
-     if(props.dish!=null){
-        
+
+};
+
+
+function RenderDish({ dish, addComment,dishId}) {
+
+    const commentlist = dish.map((comm) => {
+        return (
+            <ul className="list-unstyled" key={comm.id}>
+                <li className="justify-content-between">
+                    {comm.comment}
+                </li>
+                <li className="justify-content-between">
+                    --{comm.author}, <ReturnDate date={comm.date} />
+                </li>
+            </ul>
+
+        );
+    });
+    return (
+        <div className="col-sm col-md-5 ml-1">
+            <h4>Comments</h4>
+            {commentlist}
+            <CommentForm dishId={dishId} addComment={addComment}/>
+        </div>
+
+    );
+
+
+}
+
+function RenderMenu({ dish }) {
+
+    return (
+        <div className="col-sm col-md-5 m-1">
+            <Card >
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function ReturnDate({ date }) {
+    console.log(date)
+    const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const mon = parseInt(date.substring(5, 7));
+    var final = month[mon - 1] + ' ' + date.substring(8, 10) + ', ' + date.substring(0, 4);
+    return final;
+}
+
+const DishDetail = (props) => {
+    if(props.isLoading){
         return(
             <div className="container">
                 <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
+                    <Loading />
                 </div>
             </div>
-            <div className="row">
-                <RenderMenu dish={props.dish} />
-                <RenderDish dish={props.comments} />
-            </div>
+        );
+    }
+    else if(props.errMess){
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
             </div>
         );
-     }
-     else{
-         return(
-            <div></div>
-         );
-     }
     }
+    else if (props.dish != null) {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderMenu dish={props.dish} />
+                    <RenderDish dish={props.comments}
+                    addComment={props.addComment}
+                    dishId={props.dish.id}
+                    />
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
 
 
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,45 +3,67 @@ import Home from './HomeComponent';
 import Menu from "./MenuComponent";
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {addComment,fetchDishes} from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
 
+
+
+const mapStateToProps = state =>{
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+}
+
+const mapDispatchToProps =(dispatch) => ({
+  addComment: (dishId,rating,author,comment)=>dispatch(addComment(dishId,rating,author,comment)),
+  fetchDishes: ()=>{dispatch(fetchDishes())},
+  resetFeedbackForm: ()=>{dispatch(actions.reset('feedback'))}
+});
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-    };
+
   }
+
+  componentDidMount(){
+    this.props.fetchDishes();
+  }
+
+ 
 
 
   render() {
 
     const HomePage=()=>{
       return(
-        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
-          leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+        <Home dish={this.props.dishes.dishes.filter((dish)=>dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
+          promotion={this.props.promotions.filter((promo)=>promo.featured)[0]}
+          leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
         />
       );
     }
 
     const DishWithId = ({match})=>{
       return(
-        <DishDetail dish={this.state.dishes.filter((dish)=>dish.id=== parseInt(match.params.dishId,10))[0]}
-        comments={this.state.comments.filter((comment)=> comment.dishId===parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.dishes.filter((dish)=>dish.id=== parseInt(match.params.dishId,10))[0]}
+        isLoading={this.props.dishes.isLoading}
+        errMess={this.props.dishes.errMess}
+        comments={this.props.comments.filter((comment)=> comment.dishId===parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}
+         />
       );
     }
 
@@ -50,10 +72,10 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage}/>
-          <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
+          <Route exact path="/menu" component={()=> <Menu dishes={this.props.dishes}/>} />
           <Route path="/menu/:dishId" component={DishWithId}/>
-          <Route exact path="/contactus" component={Contact} />
-          <Route exact path="/aboutus" component={()=> <About leaders={this.state.leaders}/>}/>
+          <Route exact path="/contactus" component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+          <Route exact path="/aboutus" component={()=> <About leaders={this.props.leaders}/>}/>
           <Redirect to="/home" />
         </Switch>
         <Footer />
@@ -62,4 +84,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Main));
